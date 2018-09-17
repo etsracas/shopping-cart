db157c10a33ecef5378d852eae02fa70e718b690
etsracas <etsryacas@gmail.com>
2018-09-18 05:58:06 +0900

    add Action

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <h1>Product List</h1>
-    <ul>
+    <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif">
+    <ul v-else>
       <li v-for="product in products" :key="product.title">{{product.title}} - {{product.price}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-import shop from '@/api/shop'
 import store from '@/store/index'
 export default {
+  data () {
+    return {
+      loading: false
+    }
+  },
+  
   computed: {
     products () {
       return store.getters.availableProducts
@@ -18,9 +24,9 @@ export default {
   },
 
   created () {
-    shop.getProducts(products => {
-      store.commit('setProducts', products)
-    })
+    this.loading = true
+    store.dispatch('fetchProducts')
+      .then(() => this.loading = false)
   }
 }
 </script>
